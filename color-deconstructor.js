db5ac0fb806844e9c768c077dca4f82c132a3119
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-06-17 09:15:37 -0400

    done

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,5 +1,13 @@
-const colorDeconstructor = () => {
-
+let colorDeconstructor = (givenColor) => {
+    if (givenColor === 'orange') {
+        return "Orange is made by combining red and yellow!";
+    } else if (givenColor === 'purple') {
+        return "Purple is made by combining red and blue!";
+    } else if (givenColor === 'green') {
+        return 'Green is made by combining blue and yellow!';
+    } else {
+        return "Sorry, that's not a secondary color!";
+    }
 }
 
-module.exports = colorDeconstructor;
+module.exports = colorDeconstructor
